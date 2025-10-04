8fece28b3d4c87ba176f4e331c0eff815de5588b
Lee <leecocky@hotmail.com>
2025-10-04 08:05:58 +0200

    services

---
+++ src/scripts/showPanel.js
@@ -0,0 +1,13 @@
+function showPanel(name) {
+  const panels = document.querySelectorAll('.panel');
+  panels.forEach(panel => panel.style.display = 'none');
+
+  const target = document.querySelector(`.panel.${name}`);
+  if (target) {
+    target.style.display = 'block';
+  }
+}
+
+document.addEventListener('DOMContentLoaded', () => {
+  showPanel('massage'); // default
+});
