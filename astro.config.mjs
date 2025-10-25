7a92c2239bc5bba57c10c1d20b67fa26ffb08f9b
Lee <leecocky@hotmail.com>
2025-10-25 12:29:43 +0200

    new page bodyrite

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,7 +4,10 @@ import jopSoftwarecookieconsent from '@jop-software/astro-cookieconsent';
 import sitemap from '@astrojs/sitemap';
 
 export default defineConfig({
-  site: 'https://massageartestepona.com', 
+  site: 'https://massageartestepona.com',
+  build: {
+    assets: '_astro', // Ensures consistent asset pathing
+  },
   integrations: [
     jopSoftwarecookieconsent(),
     sitemap()
